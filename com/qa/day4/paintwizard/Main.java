a3960eeef9ccd8bfd51bd545365f3390804a2da8
Pete <PHutchison@qa.com>
2021-10-01 14:29:47 +0100

    Improved Paint wizard

--- com/qa/day4/paintwizard/Main.java
+++ com/qa/day4/paintwizard/Main.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 	
-	
+	public static void nicetype(String message) throws InterruptedException{
+		char[] chars = message.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			System.out.print(chars[i]);
+			Thread.sleep(60);			
+		}
+	}
 	
 //	This program should work out which paint is cheapest
 //	and which will result in the least waste for a given
@@ -22,28 +28,47 @@ public class Main {
 		Paint joes = new Paint("Average Joes", 16, 17.99, 11);
 		Paint fancy = new Paint("Fancy Paint", 10, 25, 22);
 		
-		PaintList newList = new PaintList();
-		newList.addPaint(cheapoMax);
-		newList.addPaint(dulux);
-		newList.addPaint(joes);
-		newList.addPaint(fancy);		
-		System.out.println(newList.toString());	
-		
-//		Scanner scan = new Scanner(System.in);
-//		System.out.println("What size area will you be painting? (in sq meters) ");
-//		String userArea = scan.nextLine();
-		
-		int myRoomArea = 750;
-		
-		System.out.println(newList.leastWaste(myRoomArea).getName());
-		System.out.println(newList.lowestCost(myRoomArea).getName());
-		
 //		System.out.println(fancy.getName());
 //		System.out.println("Number of tins = " + fancy.getNumOfTins(myRoomArea));
 //		System.out.println("Total cost = " + fancy.getCost(myRoomArea)); 
 //		System.out.println("Amount wasted in litres = " + fancy.getWaste(myRoomArea));
 //		
 //		System.out.println("\n ======================================== \n");
+		
+		PaintList newList = new PaintList();
+		newList.addPaint(cheapoMax);
+		newList.addPaint(dulux);
+		newList.addPaint(joes);
+		newList.addPaint(fancy);		
+//		System.out.println(newList.toString());	
+		
+//		int exampleRoomArea = 750;
+		
+		Scanner scan = new Scanner(System.in);
+		String message = "HELLO AND WELCOME!!! \nWhat size area will you be painting? (in sq meters)\n";
+		try {
+			nicetype(message);
+		} catch (InterruptedException e) {
+			// This shouldn't happen
+			e.printStackTrace();
+		}
+		int userArea = Integer.parseInt(scan.nextLine());
+		scan.close();
+		
+		String wasteResult = "You will have the least waste if you use " + newList.leastWaste(userArea).getName() + "!!!!! WAHOOOO!!\n";
+		String costResult = "The cheapest paint for you will be " + newList.lowestCost(userArea).getName() + "!!!!! HOORAY!!\n";
+		try {
+			nicetype(wasteResult);
+		} catch (InterruptedException e) {
+			// This shouldn't happen
+			e.printStackTrace();
+		}
+		try {
+			nicetype(costResult);
+		} catch (InterruptedException e) {
+			// This shouldn't happen
+			e.printStackTrace();
+		}
 
 	}
 
